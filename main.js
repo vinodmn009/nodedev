157eb5195d9c78897bcf282da06327fbf8217d33
root <root@den01eaw.us.oracle.com>
2018-02-06 01:37:25 -0800

    First Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// load the http module
+var http = require('http');
+
+// configure our HTTP server
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+response.write("\nversion 2 of the coe");
+  response.end("Hello, welcome to the node app built using Docker on Jenkins v2\n");
+});
+
+// listen on localhost:8000
+server.listen(8000);
+console.log("Server listening at http://127.0.0.1:8000/");var http = require('http');
+
+// configure our HTTP server
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+response.write("\nversion 2 of the coe");
+  response.end("Hello, welcome to the node app built using Docker on Jenkins v2\n");
+});
+
+// listen on localhost:8000
+server.listen(8000);
+console.log("Server listening at http://127.0.0.1:8000/");
